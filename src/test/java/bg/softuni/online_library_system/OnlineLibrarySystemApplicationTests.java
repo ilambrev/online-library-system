fefcbd4ca83416d3579f274ae13c29dd3c93e6ea
Ivan Lambrev <ilambrev@abv.bg>
2025-02-27 10:36:00 +0200

    Initial project configuration

---
+++ src/test/java/bg/softuni/online_library_system/OnlineLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.online_library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
