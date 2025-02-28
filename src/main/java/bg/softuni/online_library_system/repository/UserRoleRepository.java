3e75fb1a3d3cbb49c9eca73c191a559734a1585b
Ivan Lambrev <ilambrev@abv.bg>
2025-02-28 10:48:10 +0200

    UserRoleRepository findByRole method added

--- src/main/java/bg/softuni/online_library_system/repository/UserRoleRepository.java
+++ src/main/java/bg/softuni/online_library_system/repository/UserRoleRepository.java
@@ -1,10 +1,11 @@
 package bg.softuni.online_library_system.repository;
 
 import bg.softuni.online_library_system.model.entity.UserRoleEntity;
+import bg.softuni.online_library_system.model.enums.UserRoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRoleEntity, Long> {
-
+    UserRoleEntity findByRole(UserRoleEnum userRoleEnum);
 }
