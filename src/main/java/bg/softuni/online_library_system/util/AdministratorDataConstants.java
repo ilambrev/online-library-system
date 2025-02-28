32262d5579720b360257cd37ddb64035b6ca58bb
Ivan Lambrev <ilambrev@abv.bg>
2025-02-28 10:43:07 +0200

    AdministratorDataConstants added

---
+++ src/main/java/bg/softuni/online_library_system/util/AdministratorDataConstants.java
@@ -0,0 +1,16 @@
+package bg.softuni.online_library_system.util;
+
+import bg.softuni.online_library_system.model.enums.GenderEnum;
+
+public final class AdministratorDataConstants {
+    public static final String FIRST_NAME = "Old";
+    public static final String LAST_NAME = "Librarian";
+    public static final String USER_NAME = "admin";
+    public static final String PASSWORD = "my&1Secret";
+    public static final String EMAIL = "admin_ols@abv.bg";
+    public static final String PHONE_NUMBER = "+359812667788";
+    public static final String ADDRESS = "Sofia, Old Library, 27";
+
+    private AdministratorDataConstants() {
+    }
+}
