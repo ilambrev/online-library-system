d71d90db122c38aa8ea9544e2827423f165302e8
Ivan Lambrev <ilambrev@abv.bg>
2025-02-28 10:07:21 +0200

    UserRoleDescriptionConstants added

---
+++ src/main/java/bg/softuni/online_library_system/util/UserRoleDescriptionConstants.java
@@ -0,0 +1,10 @@
+package bg.softuni.online_library_system.util;
+
+public final class UserRoleDescriptionConstants {
+    public static final String USER_ROLE_DESCRIPTION = "View book information, write comments, borrow books, change profile details";
+    public static final String MODERATOR_ROLE_DESCRIPTION = "Manages books and user comments";
+    public static final String ADMINISTRATOR_ROLE_DESCRIPTION = "Has full control over the system - manages users, books, comments";
+
+    private UserRoleDescriptionConstants() {
+    }
+}
