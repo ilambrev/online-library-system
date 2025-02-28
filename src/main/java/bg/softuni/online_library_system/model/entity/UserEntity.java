08aec16203453e6ffb3eace21f6682e378059364
Ivan Lambrev <ilambrev@abv.bg>
2025-02-28 08:59:18 +0200

    UserEntity and GenderEnum added

---
+++ src/main/java/bg/softuni/online_library_system/model/entity/UserEntity.java
@@ -0,0 +1,121 @@
+package bg.softuni.online_library_system.model.entity;
+
+import bg.softuni.online_library_system.model.enums.GenderEnum;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    public UserEntity() {
+        this.active = false;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserEntity setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserEntity setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserEntity setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserEntity setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public UserEntity setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public UserEntity setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public GenderEnum getGender() {
+        return gender;
+    }
+
+    public UserEntity setGender(GenderEnum gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public UserEntity setActive(boolean active) {
+        this.active = active;
+        return this;
+    }
+}
---
+++ src/main/java/bg/softuni/online_library_system/model/enums/GenderEnum.java
@@ -0,0 +1,6 @@
+package bg.softuni.online_library_system.model.enums;
+
+public enum GenderEnum {
+    MALE,
+    FEMALE
+}
