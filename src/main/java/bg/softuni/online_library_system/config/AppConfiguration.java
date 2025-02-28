3632b448a2932277418b0b4b7b1f9004c358269f
Ivan Lambrev <ilambrev@abv.bg>
2025-02-28 09:15:25 +0200

    AppConfiguration with PasswordEncoder added

---
+++ src/main/java/bg/softuni/online_library_system/config/AppConfiguration.java
@@ -0,0 +1,15 @@
+package bg.softuni.online_library_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+}
