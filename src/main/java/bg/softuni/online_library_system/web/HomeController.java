e896219d7b9f6855dd67f796e398a3e93c7c2d9d
Ivan Lambrev <ilambrev@abv.bg>
2025-03-02 17:59:33 +0200

    HomeController added

---
+++ src/main/java/bg/softuni/online_library_system/web/HomeController.java
@@ -0,0 +1,14 @@
+package bg.softuni.online_library_system.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome() {
+
+        return "index";
+    }
+}
