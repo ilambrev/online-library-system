ee4be6b168b04951771a5c6a05e8046fb870b755
Ivan Lambrev <ilambrev@abv.bg>
2025-03-04 17:21:57 +0200

    Classes that contain constants moved in own package common.constant

---
+++ src/main/java/bg/softuni/online_library_system/common/constant/UserRoleDescriptionConstants.java
@@ -0,0 +1,10 @@
+package bg.softuni.online_library_system.common.constant;
+
+public final class UserRoleDescriptionConstants {
+    public static final String USER_ROLE_DESCRIPTION = "Can view book information, write comments, borrow books, change profile details";
+    public static final String STAFF_ROLE_DESCRIPTION = "Can create and edit authors, books, categories and user comments";
+    public static final String ADMIN_ROLE_DESCRIPTION = "Has full control over the system - create, edit and delete users, books, comments, etc.";
+
+    private UserRoleDescriptionConstants() {
+    }
+}
