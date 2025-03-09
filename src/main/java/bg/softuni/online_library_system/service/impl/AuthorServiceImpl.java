8dade7eaa411b8d3235692130ce4fe434ba1ceec
Ivan Lambrev <ilambrev@abv.bg>
2025-03-09 13:13:35 +0200

    Refactoring AuthorService to use ModelMapper for object mapping

--- src/main/java/bg/softuni/online_library_system/service/impl/AuthorServiceImpl.java
+++ src/main/java/bg/softuni/online_library_system/service/impl/AuthorServiceImpl.java
@@ -6,6 +6,7 @@ import bg.softuni.online_library_system.model.entity.AuthorEntity;
 import bg.softuni.online_library_system.repository.AuthorRepository;
 import bg.softuni.online_library_system.service.AuthorService;
 import bg.softuni.online_library_system.service.CloudinaryService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -16,23 +17,21 @@ import java.util.Optional;
 public class AuthorServiceImpl implements AuthorService {
     private final AuthorRepository authorRepository;
     private final CloudinaryService cloudinaryService;
+    private final ModelMapper modelMapper;
 
-    public AuthorServiceImpl(AuthorRepository authorRepository, CloudinaryService cloudinaryService) {
+    public AuthorServiceImpl(AuthorRepository authorRepository, CloudinaryService cloudinaryService, ModelMapper modelMapper) {
         this.authorRepository = authorRepository;
         this.cloudinaryService = cloudinaryService;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public boolean addAuthor(AddAuthorDTO addAuthorDTO) throws IOException {
         String directory = "authors";
         String imageUrl = this.cloudinaryService.uploadFile(addAuthorDTO.getImageFile(), directory);
-        System.out.println(imageUrl);
 
-        AuthorEntity newAuthor = new AuthorEntity()
-                .setFirstName(addAuthorDTO.getFirstName())
-                .setLastName(addAuthorDTO.getLastName())
-                .setPresentation(addAuthorDTO.getPresentation())
-                .setImageURL(imageUrl);
+        AuthorEntity newAuthor = this.modelMapper.map(addAuthorDTO, AuthorEntity.class);
+        newAuthor.setImageURL(imageUrl);
 
         this.authorRepository.save(newAuthor);
 
@@ -43,12 +42,7 @@ public class AuthorServiceImpl implements AuthorService {
     public List<AuthorDTO> getAllAuthorsOrderByFirstName() {
         List<AuthorEntity> authors = this.authorRepository.findAllByOrderByFirstNameAsc();
         return authors.stream()
-                .map(a -> new AuthorDTO()
-                        .setId(a.getId())
-                        .setFirstName(a.getFirstName())
-                        .setLastName(a.getLastName())
-                        .setPresentation(a.getPresentation())
-                        .setImageURL(a.getImageURL()))
+                .map(a -> this.modelMapper.map(a, AuthorDTO.class))
                 .toList();
     }
 
@@ -56,12 +50,7 @@ public class AuthorServiceImpl implements AuthorService {
     public List<AuthorDTO> getAllAuthors() {
         List<AuthorEntity> authors = this.authorRepository.findAll();
         return authors.stream()
-                .map(a -> new AuthorDTO()
-                        .setId(a.getId())
-                        .setFirstName(a.getFirstName())
-                        .setLastName(a.getLastName())
-                        .setPresentation(a.getPresentation())
-                        .setImageURL(a.getImageURL()))
+                .map(a -> this.modelMapper.map(a, AuthorDTO.class))
                 .toList();
     }
 
@@ -73,10 +62,6 @@ public class AuthorServiceImpl implements AuthorService {
         }
         AuthorEntity author = authorOptional.get();
 
-        return new AuthorDTO().setId(author.getId())
-                .setFirstName(author.getFirstName())
-                .setLastName(author.getLastName())
-                .setPresentation(author.getPresentation())
-                .setImageURL(author.getImageURL());
+        return this.modelMapper.map(author, AuthorDTO.class);
     }
 }
