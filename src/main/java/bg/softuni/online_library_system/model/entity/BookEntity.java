23b7b56c8e2a34c13783795b703bec748461a63f
Ivan Lambrev <ilambrev@abv.bg>
2025-03-10 20:04:57 +0200

    BookEntity added

--- src/main/java/bg/softuni/online_library_system/model/entity/BookEntity.java
+++ src/main/java/bg/softuni/online_library_system/model/entity/BookEntity.java
@@ -1,15 +1,71 @@
 package bg.softuni.online_library_system.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-//@Entity
-//@Table(name = "books")
+@Entity
+@Table(name = "books")
 public class BookEntity extends BaseEntity {
 
-    private String name;
+    private String title;
 
     private String description;
 
+    private String isbn;
 
+    private String imageURL;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private AuthorEntity author;
+
+    public BookEntity() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BookEntity setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BookEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public BookEntity setIsbn(String isbn) {
+        this.isbn = isbn;
+        return this;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public BookEntity setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+        return this;
+    }
+
+    public AuthorEntity getAuthor() {
+        return author;
+    }
+
+    public BookEntity setAuthor(AuthorEntity author) {
+        this.author = author;
+        return this;
+    }
 }
