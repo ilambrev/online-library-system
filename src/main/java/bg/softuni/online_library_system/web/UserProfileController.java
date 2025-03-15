65285cb0b5638d151056de74f7fd5e57b59fe65f
Ivan Lambrev <ilambrev@abv.bg>
2025-03-15 12:56:46 +0200

    User Profile page added

---
+++ src/main/java/bg/softuni/online_library_system/web/UserProfileController.java
@@ -0,0 +1,17 @@
+package bg.softuni.online_library_system.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+public class UserProfileController {
+
+    @GetMapping("/profile")
+    public String getUserProfile(@RequestParam String userName) {
+
+        return "profile";
+    }
+}
