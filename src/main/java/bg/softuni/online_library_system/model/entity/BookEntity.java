5a8cb8aee1edc1aa8ea1ec934be4ae1f4608f239
Ivan Lambrev <ilambrev@abv.bg>
2025-03-18 15:58:26 +0200

    BookEntity new fields added

--- src/main/java/bg/softuni/online_library_system/model/entity/BookEntity.java
+++ src/main/java/bg/softuni/online_library_system/model/entity/BookEntity.java
@@ -1,25 +1,41 @@
 package bg.softuni.online_library_system.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "books")
 public class BookEntity extends BaseEntity {
 
+    @Column(name = "title", nullable = false)
     private String title;
 
+    @Column(name = "description", nullable = false)
     private String description;
 
+    @Column(name = "isbn", nullable = false)
     private String isbn;
 
+    @Column(name = "image_url")
     private String imageURL;
 
-    @ManyToOne
-    @JoinColumn(name = "author_id")
+    @Column(name = "entry_date", nullable = false)
+    private LocalDateTime entryDate;
+
+    @Column(name = "read_counter")
+    private int readCounter;
+
+    @Column(name = "is_available")
+    private boolean isAvailable;
+
+    @ManyToOne(targetEntity = AuthorEntity.class)
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
     private AuthorEntity author;
+
+    @ManyToOne(targetEntity = BookGenreEntity.class)
+    @JoinColumn(name = "genre_id", referencedColumnName = "id")
+    private BookGenreEntity genre;
 
     public BookEntity() {
     }
@@ -60,12 +76,48 @@ public class BookEntity extends BaseEntity {
         return this;
     }
 
+    public LocalDateTime getEntryDate() {
+        return entryDate;
+    }
+
+    public BookEntity setEntryDate(LocalDateTime entryDate) {
+        this.entryDate = entryDate;
+        return this;
+    }
+
+    public int getReadCounter() {
+        return readCounter;
+    }
+
+    public BookEntity setReadCounter(int readCounter) {
+        this.readCounter = readCounter;
+        return this;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public BookEntity setAvailable(boolean available) {
+        isAvailable = available;
+        return this;
+    }
+
     public AuthorEntity getAuthor() {
         return author;
     }
 
     public BookEntity setAuthor(AuthorEntity author) {
         this.author = author;
+        return this;
+    }
+
+    public BookGenreEntity getGenre() {
+        return genre;
+    }
+
+    public BookEntity setGenre(BookGenreEntity genre) {
+        this.genre = genre;
         return this;
     }
 }
