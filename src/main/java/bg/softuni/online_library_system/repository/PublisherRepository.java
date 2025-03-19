b9900ab5c4f64eeba88d7c3d5d269fb19c8e9658
Ivan Lambrev <ilambrev@abv.bg>
2025-03-19 11:47:10 +0200

    BookRepository and PublisherRepository added

---
+++ src/main/java/bg/softuni/online_library_system/repository/PublisherRepository.java
@@ -0,0 +1,10 @@
+package bg.softuni.online_library_system.repository;
+
+import bg.softuni.online_library_system.model.entity.PublisherEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<PublisherEntity, Long> {
+
+}
