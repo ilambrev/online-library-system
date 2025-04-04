325fed434f7d10413f7c261f452d0a495200ee5f
Ivan Lambrev <ilambrev@abv.bg>
2025-04-04 11:36:08 +0300

    App time zone set to GMT+03:00

--- src/main/java/bg/softuni/online_library_system/OnlineLibrarySystemApplication.java
+++ src/main/java/bg/softuni/online_library_system/OnlineLibrarySystemApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class OnlineLibrarySystemApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+03:00"));
+
 		SpringApplication.run(OnlineLibrarySystemApplication.class, args);
 	}
-
 }
